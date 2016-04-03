a0dff60b9fdf0ce225ed397f38abbf05d9001fae
King <dannyking91@gmail.com>
2016-04-03 19:41:02 -0400

    changed js interface structure

--- www/NSDPlugin.js
+++ www/NSDPlugin.js
@@ -1,31 +1,35 @@
 var exec = require('cordova/exec');
 
-var NetworkServiceDiscovery = function() {
-
+module.exports = {
+    initializedNsd: function(arg0, success, error) {
+        console.log("initializedNsd Called!");
+        exec(success, error, "NSDPlugin", "initializeNsd", [arg0]);
+    }
 };
 
-NetworkServiceDiscovery.registerService = function(arg0, success, error) {
-    exec(success, error, "NSDPlugin", "registerService", [arg0]);
-};
+// NetworkServiceDiscovery.registerService = function(arg0, success, error) {
+//     exec(success, error, "NSDPlugin", "registerService", [arg0]);
+// };
+//
+// NetworkServiceDiscovery.discoverServices = function(arg0, success, error) {
+//     exec(success, error, "NSDPlugin", "discoverServices", [arg0]);
+// };
+//
+// NetworkServiceDiscovery.getChosenServiceInfo = function(arg0, success, error) {
+//     exec(success, error, "NSDPlugin", "getChosenServiceInfo", [arg0]);
+// };
+//
+// NetworkServiceDiscovery.stopDiscovery = function(arg0, success, error) {
+//     exec(success, error, "NSDPlugin", "stopDiscovery", [arg0]);
+// };
+//
+// NetworkServiceDiscovery.tearDown = function(arg0, success, error) {
+//     exec(success, error, "NSDPlugin", "tearDown", [arg0]);
+// };
+//
+// NetworkServiceDiscovery.initializeNsd = function(arg0, success, error) {
+//     console.log("initializedNsd Called!");
+//     exec(success, error, "NSDPlugin", "initializeNsd", [arg0]);
+// }
 
-NetworkServiceDiscovery.discoverServices = function(arg0, success, error) {
-    exec(success, error, "NSDPlugin", "discoverServices", [arg0]);
-};
-
-NetworkServiceDiscovery.getChosenServiceInfo = function(arg0, success, error) {
-    exec(success, error, "NSDPlugin", "getChosenServiceInfo", [arg0]);
-};
-
-NetworkServiceDiscovery.stopDiscovery = function(arg0, success, error) {
-    exec(success, error, "NSDPlugin", "stopDiscovery", [arg0]);
-};
-
-NetworkServiceDiscovery.tearDown = function(arg0, success, error) {
-    exec(success, error, "NSDPlugin", "tearDown", [arg0]);
-};
-
-NetworkServiceDiscovery.initializeNsd = function(arg0, success, error) {
-    exec(success, error, "NSDPlugin", "initializeNsd", [arg0]);
-}
-
-module.exports = NetworkServiceDiscovery;
+//module.exports = NetworkServiceDiscovery;
